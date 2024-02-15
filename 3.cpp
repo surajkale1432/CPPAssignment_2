f25b39754f7d3087572663c0ed80b5f553a1360b
surajkale1432 <91897995+surajkale1432@users.noreply.github.com>
2024-02-15 08:04:25 +0530

    done

--- 3.cpp
+++ 3.cpp
@@ -10,17 +10,14 @@ using namespace std;
 struct Student
 {
     float percent;
-
     void setPercent(float per)
     {
         this->percent=per;
     }
-
     float getPercent()
     {
         return this->percent;
     }
-
     Student()
     {
         this->percent=0;
@@ -37,12 +34,10 @@ struct Employee
     {
         this->salLPA=sal;
     }
-
     float getSal()
     {
         return this->salLPA;
     }
-
     Employee()
     {
         this->salLPA=0;
@@ -78,38 +73,51 @@ struct Loan
             printf("\nSorry Student is not eligible for any Loan");
         }
     }
-    // void getLoan(Employee e1)
-    // {
-    //     if(s1.getPercent()>=80)
-    //     {
-    //         printf("\nStudent is Eligible for Loan : 2,00,000/-");
-    //     }
-    //     else if(s1.getPercent()<80 && s1.getPercent()>=60)
-    //     {
-    //         printf("\nStudent is Eligible for Loan : 1,00,000/-");
-    //     }
-    //     else if(s1.getPercent()>=40 && s1.getPercent()<60)
-    //     {
-    //         printf("\nStudent is Eligible for Loan : 50,000/-");
-    //     }
-    //     else if (s1.getPercent()<40)
-    //     {
-    //         printf("\nSorry Student is not eligible for any Loan");
-    //     }
-
-    // }
+    void grantLoan(Employee e1)
+    {
+        if(e1.getSal()>=1200000)
+        {
+            loanAmmount=700000;
+        }
+        else if(e1.getSal()<1200000 && e1.getSal()>=1000000)
+        {
+            loanAmmount=600000;
+        }
+        else if(e1.getSal()>=600000 && e1.getSal()<1000000)
+        {
+            loanAmmount=500000;
+        }
+        else if(e1.getSal()<600000 && e1.getSal()>=400000)
+        {
+            loanAmmount=400000;
+        }
+        else if(e1.getSal()<400000 && e1.getSal()>1)
+        {
+            cout<<"\nSorry !!! Employee is not eligible for any Loan";
+        }
+        else{
+            cout<<"\nInvalid salary ammount entered by user...!!!";
+        }
+        if(e1.getSal()>400000)
+        {
+            cout<<"\nEmployee is eligible for loanAmmount = "<<loanAmmount;
+        }
+    }
+    
 };
 int main()
 {
     Student s1;
+    Employee e1;
     Loan l1;
-    float per;
-    // cout<<"\nEnter Percentage for Student = ";
+    float per,salaryInLPA;
+    cout<<"\nEnter Percentage for Student = ";
     cin>>per;
-    // scanf("%f",&per);
     s1.setPercent(per);
-
     l1.grantLoan(s1);
 
-
+    cout<<"\n\nEnter Employee Salary per Annum like '250000'\nsalary = ";
+    cin>>salaryInLPA;
+    e1.setSal(salaryInLPA);
+    l1.grantLoan(e1);
 }
